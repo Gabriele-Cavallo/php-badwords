e4ec01754d05bb8920cdf41f2eeae037545de31d
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-04-30 14:13:40 +0200

    feat: add str_replace

--- censored.php
+++ censored.php
@@ -1,6 +1,8 @@
 <?php
-    $uncesoredText= $_GET['inputText'];
-    $censoredWord= $_GET['inputWord'];
+    $uncesoredText = $_GET['inputText'];
+    $censoredWord = $_GET['inputWord'];
+    $sostitutiveText = '***';
+    $censoredText = str_replace($censoredWord, $sostitutiveText, $uncesoredText);
 ?>
 
 <!DOCTYPE html>
@@ -12,5 +14,7 @@
 </head>
 <body>
     <p>Il testo inserito è: <?php echo $uncesoredText; ?> e la mia lunghezza è di <?php echo strlen($uncesoredText); ?> caratteri.</p>
+    <p>La parola da censurare è: <?php echo $censoredWord; ?>  </p>
+    <p>Il testo censurato è: <?php echo $censoredText; ?> e la mia lunghezza è di <?php echo strlen($censoredText); ?> caratteri.</p>
 </body>
 </html>
